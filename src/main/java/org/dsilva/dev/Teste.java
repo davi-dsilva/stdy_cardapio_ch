cb70f1ecd45605c0a3b4e5a2f98187cfd5134875
DSilva <davi@dsilva.dev>
2026-09-15 13:49:46 -0300

    ln: if e else em java

--- src/main/java/org/dsilva/dev/Teste.java
+++ src/main/java/org/dsilva/dev/Teste.java
@@ -6,23 +6,33 @@ void main() {
     var preco = 3.50;
     var precoComDesconto = 2.99;
     var id = 3_000_000_000L;
-    int categoria = 2;
-
-
+    int categoria = 1;
 
     double porcentagemDesconto = (preco - precoComDesconto) / preco;
 
-    System.out.println("Porcentagem de Desconto: " + porcentagemDesconto);
+
     /*
-     * 1 - categorias
-     * 2 - Entradas
+     * 1 - Entradas
+     * 2 - Pratos Principais
      * 3 - Sobremesas
      * 4 - Bebidas*/
-
+    IO.println("Nome: " + nome);
+    //--------------Operadores de condicao em Java ---------------------
+    System.out.println("Porcentagem de Desconto: " + porcentagemDesconto);
     if (emPromocao){
         IO.println("Preco: de " + preco + " por " + precoComDesconto);
     } else {
         IO.print("Preco: " + preco);
+    }
+
+    if (categoria == 1){ //comparacao da variavel com o valor igual == diferente !=
+                IO.println("Categoria: Entradas");
+    }if (categoria == 2){ //comparacao da variavel com o valor igual == diferente !=
+                IO.println("Categoria: Pratos Principais");
+    }if (categoria == 3){ //comparacao da variavel com o valor igual == diferente !=
+                IO.println("Categoria: Sobremesas");
+    }if (categoria == 4){ //comparacao da variavel com o valor igual == diferente !=
+                IO.println("Categoria: Bebidas");
     }
 }
 

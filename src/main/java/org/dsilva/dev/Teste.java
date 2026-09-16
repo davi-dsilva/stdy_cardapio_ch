db5c0549b04c6254eb7d0ff393d0747a87a55ed6
DSilva <davi@dsilva.dev>
2026-09-16 20:48:17 -0300

    ln: continue em java
    obs.: o mesmo resultado do continue pode ser feito com else neste caso.

--- src/main/java/org/dsilva/dev/Teste.java
+++ src/main/java/org/dsilva/dev/Teste.java
@@ -141,9 +141,11 @@ IO.println("-".repeat(50));
     //Todos os preços menos que o Limite
 
     for (double preco : precos) {
-        if (preco <= precoLimite){
+        if (preco <= precoLimite) {
             IO.println("Preço menor que " + precoLimite + ": " + preco);
+            continue;
         }
+            IO.println("Processamento pesado para: " + preco); //só processar para valores maiores que 10
 
     }
 
